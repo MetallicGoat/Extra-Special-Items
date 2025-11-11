9e04cbf9bb5ca238174a775cebfadf34ffbf5d36
MetallicGoat <ecazzam3@gmail.com>
2025-11-11 13:38:25 -0500

    Add package javadocs for new API

---
+++ src/main/java/me/metallicgoat/specialItems/api/event/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Contains all Events fired by Extra-Special-Items
+ */
+package me.metallicgoat.specialItems.api.event;
---
+++ src/main/java/me/metallicgoat/specialItems/api/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * The root package of the Extra-Special-Items API
+ */
+package me.metallicgoat.specialItems.api;
